41dfd41314249b2bd472ba223b215fe9ce7e4ec3
Jasv06 <105725933+Jasv06@users.noreply.github.com>
2023-07-02 00:06:47 +0200

    changes

--- include/swot_manipulation_bt/shared_classes.h
+++ include/swot_manipulation_bt/shared_classes.h
@@ -8,5 +8,77 @@
 #ifndef SHARED_POSES_H
 #define SHARED_POSES_H
 
+/**
+ *      @class GetGraspAndMoveGrasp
+ *      @brief A synchronized action node for getting the grasping and moving point.
+ *      @details This class handles the logic for obtaining the grasping point and moving the robot 
+ *          arm to the desired position.
+ */
+
+class GetGraspAndMoveGrasp : public BT::SyncActionNode
+{
+    private:
+        Manipulation& manipulation_;
+        std::vector<ConditionAction> conditionActions;
+
+    public:
+        GetGraspAndMoveGrasp(const std::string& name, Manipulation& manipulation);
+        ~GetGraspAndMoveGrasp() override; 
+        virtual BT::NodeStatus tick() override;
+};
+
+/**
+ *      @class PickPlaceObject
+ *      @brief A synchronized action node for picking up or placing an object.
+ *      @details This class handles the logic for picking up an object or placing it in the grasping point.
+ */
+
+class PickPlaceObject : public BT::SyncActionNode
+{
+    private:
+        Manipulation& manipulation_;
+
+    public:
+        PickObject(const std::string& name, Manipulation& manipulation);
+        ~PickObject() override;      
+        virtual BT::NodeStatus tick() override;
+    
+};
+
+/**
+ *      @class MoveHomePos
+ *      @brief A synchronized action node for moving to the home position.
+ *      @details This class handles the logic for moving the robot to the home position depending on the 
+ *          last position of the robot.
+ */
+
+class MoveHomePos : public BT::SyncActionNode
+{
+    private:
+        Manipulation& manipulation_;
+
+    public:
+        MoveHomePos(const std::string& name, Manipulation& manipulation);
+        ~MoveHomePos() override;      
+        BT::NodeStatus tick() override;
+};
+
+/**
+ *      @class MoveToDrivePose
+ *      @brief A synchronized action node for moving to the drive pose.
+ *      @details This class handles the logic for moving the robot to the drive pose which is one the
+ *          main tasks to be perfomed.
+ */
+
+class MoveToDrivePose : public BT::SyncActionNode
+{
+    private:
+        Manipulation& manipulation_;
+
+    public:
+        MoveToDrivePose(const std::string& name, Manipulation& manipulation);
+        ~MoveToDrivePose() override;      
+        BT::NodeStatus tick() override;
+};
 
 #endif
